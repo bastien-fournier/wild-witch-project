24faea94a4a8f25120143a8aad76bf893d6bb3bf
Johana Lavigne <johana.lavigne@gmail.com>
2024-07-03 22:42:16 +0200

    feat: added icons inside assets folder

--- src/components/DropDownBtn.jsx
+++ src/components/DropDownBtn.jsx
@@ -4,6 +4,7 @@ import {
   DropdownMenu,
   DropdownToggle,
 } from "react-bootstrap";
+import { icons } from "../assets/icons.js";
 
 export default function DropDownBtn() {
   return (
@@ -16,14 +17,14 @@ export default function DropDownBtn() {
           aria-label="Toggle theme (light)"
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="currentColor"
+            xmlns={icons.xmlns}
+            width={icons.widthHeight}
+            height={icons.widthHeight}
+            fill={icons.fill}
             className="bi bi-sun-fill my-1"
-            viewBox="0 0 16 16"
+            viewBox={icons.viewBox}
           >
-            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+            <path d={icons.path["bi-sun-fill"]} />
           </svg>
         </DropdownToggle>
         <DropdownMenu>
@@ -34,25 +35,25 @@ export default function DropDownBtn() {
             href="#"
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-sun-fill me-2 opacity-50"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+              <path d={icons.path["bi-sun-fill"]} />
             </svg>{" "}
             Light
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-check2 ms-auto d-none"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+              <path d={icons.path["bi-check2"]} />
             </svg>
           </DropdownItem>
           <DropdownItem
@@ -62,26 +63,26 @@ export default function DropDownBtn() {
             href="#"
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-moon-stars-fill me-2 opacity-50"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
-              <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+              <path d={icons.path["bi-moon-stars-fill"]["1"]} />
+              <path d={icons.path["bi-moon-stars-fill"]["2"]} />
             </svg>{" "}
             Dark
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-check2 ms-auto d-none"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+              <path d={icons.path["bi-check2"]} />
             </svg>
           </DropdownItem>
           <DropdownItem
@@ -91,25 +92,25 @@ export default function DropDownBtn() {
             href="#"
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-circle-half me-2 opacity-50"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M8 15A7 7 0 1 0 8 1zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16" />
+              <path d={icons.path["bi-circle-half"]} />
             </svg>{" "}
             Auto
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
+              xmlns={icons.xmlns}
+              width={icons.widthHeight}
+              height={icons.widthHeight}
+              fill={icons.fill}
               className="bi bi-check2 ms-auto d-none"
-              viewBox="0 0 16 16"
+              viewBox={icons.viewBox}
             >
-              <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+              <path d={icons.path["bi-check2"]} />
             </svg>
           </DropdownItem>
         </DropdownMenu>
