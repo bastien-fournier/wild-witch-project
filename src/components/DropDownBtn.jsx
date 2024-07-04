6891def51b94e40364b643567a8de55266008135
Johana Lavigne <johana.lavigne@gmail.com>
2024-07-04 16:17:02 +0200

    feat: ongoing modifications on theme

--- src/components/DropDownBtn.jsx
+++ src/components/DropDownBtn.jsx
@@ -5,47 +5,68 @@ import {
   DropdownToggle,
 } from "react-bootstrap";
 import { icons } from "../assets/icons.js";
+import { useState } from "react";
 
 export default function DropDownBtn() {
+  const [selectedTheme, setSelectedTheme] = useState("auto");
+
+  const handleThemeChange = (theme) => {
+    setSelectedTheme(theme);
+  };
+
+  const themeIcons = {
+    light: "bi-sun-fill",
+    dark: "bi-moon-stars-fill",
+    auto: "bi-circle-half",
+  };
+
+  console.log(themeIcons["dark"]);
+  const getPath = (iconName) => {
+    if (iconName === themeIcons["dark"]) {
+      return (
+        <>
+          <path d={icons.path[themeIcons["dark"]]["1"]} />
+          <path d={icons.path[themeIcons["dark"]]["2"]} />
+        </>
+      );
+    }
+    return <path d={icons.path[iconName]} />;
+  };
+
+  console.log(icons.path[themeIcons[selectedTheme]]);
+
   return (
     <>
-      {["light", "dark", "auto"].map((theme, key) => {
-        const isActive = theme === "auto";
-        const themeIcons = {
-          light: "bi-sun-fill",
-          dark: "bi-moon-stars-fill",
-          auto: "bi-circle-half",
-        };
-        console.log("isActive: ", isActive);
-        console.log("themeIcons: ", themeIcons);
-        return (
-          <Dropdown
-            className="position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle"
-            key={key}
+      <Dropdown className="position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+        <DropdownToggle
+          variant="primary"
+          className="py-2 d-flex align-items-center show"
+          aria-expanded="true"
+          data-bs-toggle="dropdown"
+          aria-label={`Toggle theme (${selectedTheme})`}
+        >
+          <svg
+            xmlns={icons.xmlns}
+            width={icons.widthHeight}
+            height={icons.widthHeight}
+            fill={icons.fill}
+            className={`bi ${themeIcons[selectedTheme]} my-1`}
+            viewBox={icons.viewBox}
           >
-            <DropdownToggle
-              variant="primary"
-              className="py-2 d-flex align-items-center show"
-              aria-expanded={isActive}
-              data-bs-toggle="dropdown"
-              aria-label="Toggle theme (auto)"
-            >
-              <svg
-                xmlns={icons.xmlns}
-                width={icons.widthHeight}
-                height={icons.widthHeight}
-                fill={icons.fill}
-                className="bi bi-sun-fill my-1"
-                viewBox={icons.viewBox}
-              >
-                <path d={icons.path["bi-sun-fill"]} />
-              </svg>
-            </DropdownToggle>
-            <DropdownMenu>
+            {getPath(themeIcons[selectedTheme])}
+            <path d={icons.path[themeIcons[selectedTheme]]} />
+          </svg>
+        </DropdownToggle>
+        <DropdownMenu>
+          {["light", "dark", "auto"].map((theme) => {
+            const isActive = theme === selectedTheme;
+            return (
               <DropdownItem
-                className="d-flex align-items-center"
+                className={`d-flex align-items-center ${isActive ? "active" : ""}`}
                 data-bs-theme-value={theme}
                 aria-pressed={isActive}
+                onClick={() => handleThemeChange(theme)}
+                key={theme}
               >
                 <svg
                   xmlns={icons.xmlns}
@@ -55,14 +76,7 @@ export default function DropDownBtn() {
                   className={`bi ${themeIcons[theme]} me-2 opacity-50`}
                   viewBox={icons.viewBox}
                 >
-                  <path d={icons.path[themeIcons[theme]]["1"]} />
-                  {
-                    icons.path[
-                      themeIcons[theme]["2"] && (
-                        <path d={icons.path[themeIcons[theme]]["2"]} />
-                      )
-                    ]
-                  }
+                  {getPath(themeIcons[selectedTheme])}
                 </svg>{" "}
                 {theme.charAt(0).toUpperCase() + theme.slice(1)}
                 <svg
@@ -76,96 +90,96 @@ export default function DropDownBtn() {
                   <path d={icons.path["bi-check2"]} />
                 </svg>{" "}
               </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        );
-      })}
+            );
+          })}
 
-      {/*<DropdownItem*/}
-      {/*  className="d-flex align-items-center"*/}
-      {/*  data-bs-theme-value="light"*/}
-      {/*  aria-pressed="false"*/}
-      {/*  href="#"*/}
-      {/*>*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-sun-fill me-2 opacity-50"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-sun-fill"]} />*/}
-      {/*  </svg>{" "}*/}
-      {/*  Light*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-check2 ms-auto d-none"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-check2"]} />*/}
-      {/*  </svg>*/}
-      {/*</DropdownItem>*/}
-      {/*<DropdownItem*/}
-      {/*  className="d-flex align-items-center"*/}
-      {/*  data-bs-theme-value="dark"*/}
-      {/*  aria-pressed="false"*/}
-      {/*  href="#"*/}
-      {/*>*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-moon-stars-fill me-2 opacity-50"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-moon-stars-fill"]["1"]} />*/}
-      {/*    <path d={icons.path["bi-moon-stars-fill"]["2"]} />*/}
-      {/*  </svg>{" "}*/}
-      {/*  Dark*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-check2 ms-auto d-none"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-check2"]} />*/}
-      {/*  </svg>*/}
-      {/*</DropdownItem>*/}
-      {/*<DropdownItem*/}
-      {/*  className="d-flex align-items-center active"*/}
-      {/*  data-bs-theme-value="auto"*/}
-      {/*  aria-pressed="true"*/}
-      {/*  href="#"*/}
-      {/*>*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-circle-half me-2 opacity-50"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-circle-half"]} />*/}
-      {/*  </svg>{" "}*/}
-      {/*  Auto*/}
-      {/*  <svg*/}
-      {/*    xmlns={icons.xmlns}*/}
-      {/*    width={icons.widthHeight}*/}
-      {/*    height={icons.widthHeight}*/}
-      {/*    fill={icons.fill}*/}
-      {/*    className="bi bi-check2 ms-auto d-none"*/}
-      {/*    viewBox={icons.viewBox}*/}
-      {/*  >*/}
-      {/*    <path d={icons.path["bi-check2"]} />*/}
-      {/*  </svg>*/}
-      {/*</DropdownItem>*/}
+          {/*<DropdownItem*/}
+          {/*  className="d-flex align-items-center"*/}
+          {/*  data-bs-theme-value="light"*/}
+          {/*  aria-pressed="false"*/}
+          {/*  href="#"*/}
+          {/*>*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-sun-fill me-2 opacity-50"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-sun-fill"]} />*/}
+          {/*  </svg>{" "}*/}
+          {/*  Light*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-check2 ms-auto d-none"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-check2"]} />*/}
+          {/*  </svg>*/}
+          {/*</DropdownItem>*/}
+          {/*<DropdownItem*/}
+          {/*  className="d-flex align-items-center"*/}
+          {/*  data-bs-theme-value="dark"*/}
+          {/*  aria-pressed="false"*/}
+          {/*  href="#"*/}
+          {/*>*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-moon-stars-fill me-2 opacity-50"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-moon-stars-fill"]["1"]} />*/}
+          {/*    <path d={icons.path["bi-moon-stars-fill"]["2"]} />*/}
+          {/*  </svg>{" "}*/}
+          {/*  Dark*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-check2 ms-auto d-none"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-check2"]} />*/}
+          {/*  </svg>*/}
+          {/*</DropdownItem>*/}
+          {/*<DropdownItem*/}
+          {/*  className="d-flex align-items-center active"*/}
+          {/*  data-bs-theme-value="auto"*/}
+          {/*  aria-pressed="true"*/}
+          {/*  href="#"*/}
+          {/*>*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-circle-half me-2 opacity-50"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-circle-half"]} />*/}
+          {/*  </svg>{" "}*/}
+          {/*  Auto*/}
+          {/*  <svg*/}
+          {/*    xmlns={icons.xmlns}*/}
+          {/*    width={icons.widthHeight}*/}
+          {/*    height={icons.widthHeight}*/}
+          {/*    fill={icons.fill}*/}
+          {/*    className="bi bi-check2 ms-auto d-none"*/}
+          {/*    viewBox={icons.viewBox}*/}
+          {/*  >*/}
+          {/*    <path d={icons.path["bi-check2"]} />*/}
+          {/*  </svg>*/}
+          {/*</DropdownItem>*/}
+        </DropdownMenu>
+      </Dropdown>
     </>
   );
 }
