a9c2b2ebf9a8e865c117f52c4b6430f4e23f0b2c
Bastien Fournier <bastien.fournier06@gmail.com>
2024-07-04 11:57:13 +0200

    add componants to page data

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,10 +1,25 @@
-import Buttons from "../components/Buttons";
+import { Col, Container, Row } from "react-bootstrap";
+import Buttons from "../components/Buttons.jsx";
+import CardInfo from "../components/CardInfo";
 
 export default function DataPage() {
+  const title = "Card Title";
+  const text =
+    "Some quick example text to build on the card title and make up the\n" +
+    "          bulk of the card's content.";
   return (
-    <div>
-      <Buttons text="Précedent" link="/info" />
-      <Buttons text="Suivant" link="/sansibilisation" />
-    </div>
+    <>
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <CardInfo title={title} text={text} />
+        </Row>
+        <Row className="justify-content-center align-items-center flex-column mt-3">
+          <Col className="text-center">
+            <Buttons text="Précédent" link="/" />
+            <Buttons text="Suivant" link="/sansibilisation" />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
