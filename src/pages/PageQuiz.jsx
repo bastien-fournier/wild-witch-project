3df7f45d31badc78a7a5e797659c46b19dc64445
Johana Lavigne <johana.lavigne@gmail.com>
2024-07-05 09:53:32 +0200

    photo added

--- src/pages/PageQuiz.jsx
+++ src/pages/PageQuiz.jsx
@@ -5,8 +5,8 @@ import "./PageQuiz.css";
 // Page principale du quiz intégrant les deux composants ci-dessus
 export default function PageQuiz() {
   return (
-    <div className="card-container">
-      <h1>Quiz</h1>
+    <div className="card-container ">
+      <h1 className="text-center mt-5">Quiz</h1>
       {/* <ProgressBar /> */}
       <Quiz />
     </div>
